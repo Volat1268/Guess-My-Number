31f1ad6406a2d8f289db0e9a9ab7d1c21dbf9f15
aleksandr_markhel <volatpy@gmail.com>
2023-06-16 19:24:36 +0300

    dom and querySelector
    first cods with html elements.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector('.message').textContent = "🐷 Correct number!";
+
+document.querySelector(".score").textContent = "50";
+document.querySelector(".number").textContent = "??";
+document.querySelector(".guess").value = 25
+
