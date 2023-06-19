4a76cd368ae1ed07c48c9d2d92c8cec6080ae696
aleksandr_markhel <volatpy@gmail.com>
2023-06-19 20:28:21 +0300

    addEventListener method to .guess added

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 'use strict';
-
+/*
 console.log(document.querySelector(".message").textContent);
 
 document.querySelector('.message').textContent = "🐷 Correct number!";
 
 document.querySelector(".score").textContent = "50";
 document.querySelector(".number").textContent = "??";
-document.querySelector(".guess").value = 25
+document.querySelector(".guess").value = 14;
+console.log(document.querySelector(".guess").value);
+*/
+document.querySelector(".check").addEventListener("click", function () {
+	const guess = document.querySelector(".guess").value;
+	console.log(guess);		
+});
 
