6dd14ea7578464b45a8cd8d84e962742901d4bde
aleksandr_markhel <volatpy@gmail.com>
2023-06-19 21:08:22 +0300

    to few added

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ document.querySelector(".number").textContent = "??";
 document.querySelector(".guess").value = 14;
 console.log(document.querySelector(".guess").value);
 */
-const secretNumber = Math.trunc(Math.random() * 20);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
 	const guess = Number(document.querySelector(".guess").value);
 	console.log(guess, typeof guess);	
 	if (!guess) {
 		document.querySelector(".message").textContent = "⚠ No number!"
+	}  else if (guess === secretNumber) {
+		document.querySelector(".message").textContent = "🎉 Correct Number!"
+	} else if (guess < secretNumber) {
+		document.querySelector(".message").textContent = "😨 Too few!"
 	}
 });
 
