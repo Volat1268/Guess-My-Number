c72033dd466e165047f036372043b5e0c03788c4
Aleks_work <alemarkhel@yandex.ru>
2023-06-20 10:07:39 +0300

    if score > 0 added
    описание внесенных изменений вносится через строку.
    еще одна линия описания.

--- script.js
+++ script.js
@@ -12,15 +12,41 @@ console.log(document.querySelector(".guess").value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
+let score = 20;
+document.querySelector(".score").textContent = score;
+
+let highscore = 0;
+document.querySelector('.highscore').textContent = highscore;
+
 document.querySelector(".check").addEventListener("click", function () {
 	const guess = Number(document.querySelector(".guess").value);
 	console.log(guess, typeof guess);	
 	if (!guess) {
 		document.querySelector(".message").textContent = "⚠ No number!"
 	}  else if (guess === secretNumber) {
-		document.querySelector(".message").textContent = "🎉 Correct Number!"
+		document.querySelector(".message").textContent = "🎉 Correct Number!";
+		if (score > highscore) {
+			highscore = score;
+			document.querySelector(".highscore").textContent = highscore;
+		}
 	} else if (guess < secretNumber) {
-		document.querySelector(".message").textContent = "😨 Too few!"
+		if (score > 1) {
+			document.querySelector(".message").textContent = "📉 Too low!";
+			score --;
+			document.querySelector(".score").textContent = score;
+		} else {
+			document.querySelector(".message").textContent = "💥 You lost ghe game!";
+			document.querySelector(".score").textContent = 0;
+		}	
+	} else if (guess > secretNumber) {
+		if (score > 0) {
+			document.querySelector('.message').textContent = '📈 Too high!';
+			score --;
+			document.querySelector(".score").textContent = score;
+		} else {
+			document.querySelector(".message").textContent = "💥 You lost ghe game!";
+			document.querySelector('.score').textContent = 0;
+		}
 	}
 });
 
