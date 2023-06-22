2998e6659ba5372ea1e698a7c2519fbcf5877ac5
Aleks_work <alemarkhel@yandex.ru>
2023-06-22 10:27:10 +0300

    to merge

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ document.querySelector(".guess").value = 14;
 console.log(document.querySelector(".guess").value);
 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+// document.querySelector(".number").textContent = secretNumber;
+console.log(`secretNumber: ${secretNumber}`);
 
 let score = 20;
 document.querySelector(".score").textContent = score;
@@ -20,8 +21,7 @@ document.querySelector('.highscore').textContent = highscore;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-
+  
   // ---- when there is no Number
   if (!guess) {
     document.querySelector('.message').textContent = '⚠ No number!';
@@ -31,7 +31,8 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
 		// change background color and width of number
 		document.querySelector('body').style.backgroundColor = '#60b347';
-		document.querySelector(".number").style.width = "40rem";
+		document.querySelector(".number").style.width = "30rem";
+		document.querySelector(".number").textContent = secretNumber;
     // if (score > highscore) {
     // 	highscore = score;
     // 	document.querySelector(".highscore").textContent = highscore;
